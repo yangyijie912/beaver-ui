3f306ddae2674deee32a5321ba4ad2f22adeafc3
yangyj <84811040@qq.com>
2025-12-13 06:11:25 +0800

    style:更换svg图标

--- src/icons/Eye.tsx
+++ src/icons/Eye.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 
 export const Eye = (props: React.SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path
-      d="M12 5c-7 0-11 7-11 7s4 7 11 7 11-7 11-7-4-7-11-7z"
-      stroke="currentColor"
-      strokeWidth={1.2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <circle cx="12" cy="12" r="2.5" fill="currentColor" />
+    <path fill="currentColor" d="M12 5c-7 0-11 7-11 7s4 7 11 7 11-7 11-7-4-7-11-7zm0 12a5 5 0 110-10 5 5 0 010 10z" />
+    <circle fill="currentColor" cx="12" cy="12" r="2.5" />
   </svg>
 );
 
--- src/icons/Trash.tsx
+++ src/icons/Trash.tsx
@@ -2,22 +2,7 @@ import React from 'react';
 
 export const Trash = (props: React.SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M3 7h18" stroke="currentColor" strokeWidth={1.2} strokeLinecap="round" strokeLinejoin="round" />
-    <path
-      d="M8 7l1 12a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2l1-12"
-      stroke="currentColor"
-      strokeWidth={1.2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path d="M10 11v6M14 11v6" stroke="currentColor" strokeWidth={1.2} strokeLinecap="round" strokeLinejoin="round" />
-    <path
-      d="M9 7V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"
-      stroke="currentColor"
-      strokeWidth={1.2}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <path fill="currentColor" d="M6 7h12v13a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V7zm3-4h6l1 1h4v2H2V4h4l1-1z" />
   </svg>
 );
 
